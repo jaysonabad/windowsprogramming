8f510dfdbfb8df5af09742c9403571dabf05044d
Jayson Abad <jayson_abad@outlook.com>
2024-11-09 04:04:13 +0800

    ...

---
+++ makefile
@@ -0,0 +1,26 @@
+
+path := ..\windowstemp
+appname := windowstemp
+bin := $(path)\bin
+bin_object := $(path)\bin\object
+source := $(path)\source
+
+c_flags := -O3 -Wall -std=c++1z -static-libstdc++ -static-libgcc
+windows_flag := -mwindows
+link := -lgdi32 -luser32 -lkernel32
+
+object := winmain.o
+
+build: $(object)
+	g++ -s $(c_flags) $(windows_flag) \
+			$(bin_object)/winmain.o \
+			-o $(bin)/windowstemp
+
+winmain.o:
+	g++ -c $(source)/winmain.cpp -o $(bin_object)/winmain.o
+
+run:
+	$(bin)/windowstemp
+
+clean:
+	del $(bin)\*.exe $(bin_object)\*.o
---
+++ source/winmain.cpp
@@ -0,0 +1,80 @@
+#include <windows.h>
+
+#pragma args n used
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+  static LPCTSTR szAppName = "MyApp";
+  WNDCLASSEX window;
+  window.cbSize = sizeof(WNDCLASSEX);
+  window.style = CS_HREDRAW | CS_VREDRAW;
+  window.lpfnWndProc = WindowProc;
+  window.cbClsExtra = 0;
+  window.cbWndExtra = 0;
+  window.hInstance = hInstance;
+  window.hIcon = LoadIcon(0, IDI_APPLICATION);
+  window.hCursor = LoadCursor(0, IDC_ARROW);
+  window.hbrBackground = static_cast<HBRUSH>(GetStockObject(GRAY_BRUSH));
+  window.lpszMenuName = 0;
+  window.lpszClassName = szAppName;
+  window.hIconSm = 0;
+
+  RegisterClassEx(&window);
+
+  HWND hWnd;
+  hWnd = CreateWindow(
+    szAppName,
+    "A Basic Window the Hard Way",
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    0,
+    0,
+    hInstance,
+    0
+  );
+
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
+
+  MSG msg;
+  while(GetMessage(&msg, 0, 0, 0) == TRUE) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
+  return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+  HDC hDC;
+  PAINTSTRUCT sPaint;
+  RECT aRect;
+
+  switch (msg) {
+    case WM_PAINT:
+      hDC = BeginPaint(hWnd, &sPaint);
+      GetClientRect(hWnd, &aRect);
+      SetBkMode(hDC, TRANSPARENT);
+      DrawText(
+        hDC,
+        "Hello, Windows!",
+        -1,
+        &aRect,
+        DT_SINGLELINE | DT_CENTER | DT_VCENTER
+      );
+      EndPaint(hWnd, &sPaint);
+      return 0;
+
+    case WM_DESTROY:
+      PostQuitMessage(WM_QUIT);
+      return 0;
+    default:
+      return DefWindowProc(hWnd, msg, wParam, lParam);
+  }
+}
